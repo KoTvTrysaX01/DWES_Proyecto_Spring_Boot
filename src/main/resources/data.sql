3ba3078d063f2e7a5ecc2c9847d949706d7f3530
Elshin Vadim <elshinvadim@gmail.com>
2026-02-04 11:36:41 +0100

    SQL Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,63 @@
+INSERT INTO roles (id, name, description) VALUES (1, 'ADMIN', 'Administrador del sistema con todos los permisos');
+
+ALTER TABLE roles ALTER COLUMN id RESTART WITH (SELECT COALESCE(MAX(id), 0) + 1 FROM roles);
+
+
+
+INSERT INTO categorias (id, categoria, descipcion) VALUES (1, 'ice_cream', 'Es un alimento congelado que por lo general se hace de productos lácteos tales como leche o crema.', './ice_creams.png');
+INSERT INTO categorias (id, categoria, descipcion) VALUES (2, 'smoothies', 'Es una bebida cremosa no alcohólica preparada a base de trozos y zumos de fruta, concentrados o congelados, mezclados tradicionalmente con productos lácteos, hielo o helado.', './smoothies.png');
+INSERT INTO categorias (id, categoria, descipcion) VALUES (3, 'milkshakes', 'El milkshake se basa en helado y leche, mientras que el batido se centra en frutas, verduras y líquidos más ligeros como el yogur o la leche vegetal.', './milkshakes.png');
+
+ALTER TABLE categorias ALTER COLUMN id RESTART WITH (SELECT COALESCE(MAX(id), 0) + 1 FROM categorias);
+
+
+
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (1, '1-ice_cream', 'descripcion', 4.99, true, './1-ice_cream.png', 1);
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (2, '2-ice_cream', 'descripcion', 3.99, true, './2-ice_cream.png', 1);
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (3, '3-ice_cream', 'descripcion', 2.99, false, './3-ice_cream.png', 1);
+
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (4, '1-smoothie', 'descripcion', 4.59, true, './1-smoothie.png', 2);
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (5, '2-smoothie', 'descripcion', 3.59, false, './2-smoothie.png', 2);
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (6, '3-smoothie', 'descripcion', 2.59, true, './3-smoothie.png', 2);
+
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (7, '1-milkshake', 'descripcion', 4.00, false, './1-milkshake.png', 3);
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (8, '2-milkshake', 'descripcion', 3.00, true, './2-milkshake.png', 3);
+INSERT INTO productos (id, nombre, descripcion, precio, stock, imagen, categoria_id) VALUES (9, '3-milkshake', 'descripcion', 2.00, false, './3-milkshake.png', 3);
+
+ALTER TABLE productos ALTER COLUMN id RESTART WITH (SELECT COALESCE(MAX(id), 0) + 1 FROM productos);
+
+
+
+INSERT INTO users_security (id, username, email, password, administrador, usuario, invitado, activado) VALUES (1, 'vadim', 'vadels@alu.edu.gva.es', '1234', true, true, false, true);
+INSERT INTO users_security (id, username, email, password, administrador, usuario, invitado, activado) VALUES (2, 'reis', 'reis@alu.edu.gva.es', '1234', true, true, false, true);
+INSERT INTO users_security (id, username, email, password, administrador, usuario, invitado, activado) VALUES (3, 'user', 'user@alu.edu.gva.es', '1234', false, true, false, true);
+
+ALTER TABLE users_security ALTER COLUMN id RESTART WITH (SELECT COALESCE(MAX(id), 0) + 1 FROM users_security);
+
+
+
+INSERT INTO pedidos (id, pedido, precio_total, tel, direccion, pedido_date, user_id) VALUES (1, '1-ice_cream x 3-ice_cream', 12.97, '123456789', 'Mi direccion', '01-12-2015', 1);
+INSERT INTO pedidos (id, pedido, precio_total, tel, direccion, pedido_date, user_id) VALUES (2, '1-smoothie x 2-milkshake', 43.77, '123456789', 'Mi direccion', '01-12-2015', 2);
+INSERT INTO pedidos (id, pedido, precio_total, tel, direccion, pedido_date, user_id) VALUES (3, '2-milkshake x 3-ice_cream', 5.99, '123456789', 'Mi direccion', '01-12-2015', 3);
+
+ALTER TABLE pedidos ALTER COLUMN id RESTART WITH (SELECT COALESCE(MAX(id), 0) + 1 FROM pedidos);
+
+
+INSERT INTO lineas_pedido (id_pedido, id_producto, cantidad, precio) VALUES (1, 1, 2, 4.99);
+INSERT INTO lineas_pedido (id_pedido, id_producto, cantidad, precio) VALUES (1, 3, 1, 2.99);
+
+INSERT INTO lineas_pedido (id_pedido, id_producto, cantidad, precio) VALUES (2, 4, 3, 4.59);
+INSERT INTO lineas_pedido (id_pedido, id_producto, cantidad, precio) VALUES (2, 8, 10, 3.00);
+
+INSERT INTO lineas_pedido (id_pedido, id_producto, cantidad, precio) VALUES (3, 8, 1, 3.00);
+INSERT INTO lineas_pedido (id_pedido, id_producto, cantidad, precio) VALUES (3, 3, 1, 2.99);
+
+ALTER TABLE lineas_pedido ALTER COLUMN id RESTART WITH (SELECT COALESCE(MAX(id), 0) + 1 FROM lineas_pedido);
+
+
+INSERT INTO mensajes (id, titulo, mensaje, email, post_date) VALUES (1, '1-Titulo', 'Mi1 Mensaje', 'example@gmail.com', '01-11-2025');
+INSERT INTO mensajes (id, titulo, mensaje, email, post_date) VALUES (2, '2-Titulo', 'Mi2 Mensaje', 'example@gmail.com', '01-11-2025');
+INSERT INTO mensajes (id, titulo, mensaje, email, post_date) VALUES (3, '3-Titulo', 'Mi3 Mensaje', 'example@gmail.com', '01-11-2025');
+
+ALTER TABLE mensajes ALTER COLUMN id RESTART WITH (SELECT COALESCE(MAX(id), 0) + 1 FROM mensajes);
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,65 @@
+CREATE TABLE IF NOT EXISTS categorias(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    categoria   VARCHAR(50) UNIQUE NOT NULL,
+    descripcion VARCHAR(200),
+    imagen      VARCHAR(100)
+)
+
+CREATE TABLE IF NOT EXISTS productos(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    nombre      VARCHAR(50) UNIQUE NOT NULL,
+    descripcion VARCHAR(200),
+    precio      DECIMAL(5,2) NOT NULL,
+    stock       BOOLEAN,
+    imagen      VARCHAR(100),
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE SET NULL
+)
+
+CREATE TABLE IF NOT EXISTS users_security (
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    username        VARCHAR(50) UNIQUE NOT NULL,
+    email           VARCHAR(100) UNIQUE NOT NULL,
+    password        VARCHAR(200) NOT NULL,
+    administrador   BOOLEAN NOT NULL,
+    usuario         BOOLEAN NOT NULL,
+    invitado        BOOLEAN NOT NULL,
+    activado        BOOLEAN NOT NULL
+)
+
+
+CREATE TABLE IF NOT EXISTS pedidos(
+    id              INT AUTO_INCREMENT PRIMARY KEY,
+    pedido          VARCHAR(300) NOT NULL,
+    precio_total    DECIMAL(5,2) NOT NULL,
+    tel             VARCHAR(10) NOT NULL,
+    direccion       VARCHAR(150) NOT NULL,
+    pedido_date     DATE NOT NULL,
+    user_id         INT,
+    FOREIGN KEY (user_id) REFERENCES users_security(id) ON DELETE SET NULL
+)
+
+CREATE TABLE IF NOT EXISTS lineas_pedido(
+    id_pedido       INT,
+    id_producto     INT,
+    precio          DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (id_pedido, id_producto)
+    FOREIGN KEY (id_pedido) REFERENCES pedidos(id) ON DELETE SET NULL,
+    FOREIGN KEY (id_producto) REFERENCES productos(id) ON DELETE SET NULL
+)
+
+CREATE TABLE IF NOT EXISTS mensajes(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    titulo      VARCHAR(30) NOT NULL,
+    mensajes    VARCHAR(300) NOT NULL,
+    email       VARCHAR(50) NOT NULL,
+    post_date   DATE NOT NULL    
+)
+
+-- CREATE TABLE IF NOT EXISTS reviews(
+--     id          INT AUTO_INCREMENT PRIMARY KEY,
+--     review      VARCHAR(300) NOT NULL,
+--     post_date   DATE NOT NULL,
+--     user_id     INT,
+--     FOREIGN KEY (user_id) REFERENCES users_security(id) ON DELETE SET NULL
+-- )
