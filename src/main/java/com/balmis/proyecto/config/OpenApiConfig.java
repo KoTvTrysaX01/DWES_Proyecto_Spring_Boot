7bda4969ab807f0308cffe810cf967793c3a751c
Elshin Vadim <elshinvadim@gmail.com>
2026-02-06 16:50:31 +0100

    Config & Swagger & CunstomUserDetailsService from riconet

---
+++ src/main/java/com/balmis/proyecto/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.balmis.proyecto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+    
+    // http://localhost:8080/proyecto
+    // http://localhost:8080/proyecto/swagger-ui.html
+    // http://localhost:8080/proyecto/api-docs
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Productos")
+                        .version("1.0.0")
+                        .description("API REST para gestión de productos")
+                        .contact(new Contact()
+                                .name("Balmis")
+                                .email("admin@balmis.com")
+                                .url("https://www.bamis.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                        .termsOfService("https://www.balmis.com/terminos"));
+        
+
+    }
+}
